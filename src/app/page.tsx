de10f8dae07fc92af6ee0441c83a9981e4cef38f
ikeraller <allerunax@gmail.com>
2026-05-08 00:15:08 +0200

    chore: mover ScrollMorphHero antes de Cómo funciona

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ export default function HomePage() {
       <section id="solucion">
         <CinematicHero />
       </section>
+      <ScrollMorphHero />
       <HowItWorks />
       <Profiles />
       <Benefits />
@@ -26,7 +27,6 @@ export default function HomePage() {
       <Testimonials />
       <FAQs />
       <CTAFinal />
-      <ScrollMorphHero />
       <Footer />
     </main>
   );
